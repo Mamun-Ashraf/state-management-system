1f13440d7d9b5a9aff8c904d9561e8eafe83d821
Mamun-Ashrsf <ashrafdkia@gmail.com>
2023-07-10 00:28:50 +0600

    updated form

--- src/Components/ReducerState/ReducerState.js
+++ src/Components/ReducerState/ReducerState.js
@@ -20,9 +20,13 @@ const ReducerState = () => {
             <h1 className='mb-7 text-lg font-medium'>Count : {state}</h1>
 
             <div className=''>
-                <button onClick={() => dispatch({ type: "DECREMENT", payload: 3 })} type="button" className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-5 mb-2">Decrement</button>
+                <button
+                    onClick={() => dispatch({ type: "DECREMENT", payload: 3 })} type="button"
+                    className="text-white bg-gradient-to-r from-red-400 via-red-500 to-red-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-5 mb-2">Decrement</button>
 
-                <button onClick={() => dispatch({ type: "INCREMENT", payload: 5 })} type="button" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Increment</button>
+                <button
+                    onClick={() => dispatch({ type: "INCREMENT", payload: 5 })} type="button"
+                    className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Increment</button>
             </div>
 
         </div>
